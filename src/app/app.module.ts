22a04b0ab7d92efee6f7638de1c148ce5ec6769b
Manas Sinkar <manas.sinkar@gmail.com>
2019-10-30 15:33:15 +0530

    Basic Components & Services Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ViewPolicyComponent } from './view-policy/view-policy.component';
+import { AddPolicyComponent } from './add-policy/add-policy.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ViewPolicyTableComponent } from './view-policy-table/view-policy-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ViewPolicyComponent,
+    AddPolicyComponent,
+    ViewProfileComponent,
+    DashboardComponent,
+    ViewPolicyTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/view-policy-table/view-policy-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-policy-table',
+  templateUrl: './view-policy-table.component.html',
+  styleUrls: ['./view-policy-table.component.css']
+})
+export class ViewPolicyTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
