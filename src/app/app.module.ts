2c903ffedf9e9d2028a576326348382d6148d687
Parth Thosani <thosaniparth@gmail.com>
2019-10-31 00:06:13 +0530

    Register and Login Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -10,6 +11,9 @@ import { AddPolicyComponent } from './add-policy/add-policy.component';
 import { ViewProfileComponent } from './view-profile/view-profile.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ViewPolicyTableComponent } from './view-policy-table/view-policy-table.component';
+import { ReactiveFormsModule , FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -24,7 +28,11 @@ import { ViewPolicyTableComponent } from './view-policy-table/view-policy-table.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+  login(username: string, password:string)
+  {
+    return this.http.post<any>(`http://localhost:3000/user/login`,{ username, password }).pipe(map(user =>
+      {
+        localStorage.setItem('currentUser', JSON.stringify(user));
+        return user;
+      }));
+  }
+
+  register(username: string, password:string, contact:string, email:string,age:Number,annualIncome:Number)
+  {
+    return this.http.post<any>(`http://localhost:3000/user/register`,{ username, email, contact, age, annualIncome, password }).pipe(map(user =>
+      {
+        return user;
+      }));
+  }
+
+}
